5abd477c7b4097e35f65d7b85400bf58783a02c5
MGVK <mukylike@yandex.ru>
2021-11-07 14:29:42 +0300

    upd

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd $1
+
 mvn clean package
 
-java -jar target/SimpleGateway-1.0-SNAPSHOT-jar-with-dependencies.jar &
+java -jar target/SimpleGateway-1.0-SNAPSHOT-jar-with-dependencies.jar 2>1 >> log &
 
 disown
